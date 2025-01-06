b0538c94ea316bf28cf836ddae13e49f9fa4bfdf
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-07 00:31:48 +0545

    feat: add middleware setup, MongoDB connection, and user routes to server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import cors from "cors";
 
+import UserRouter from "./routes/user.js";
+import connection from "./connection.js";
+
 const app = express();
 const port = 3000;
 
+// Middleware setput
+app.use(express.json());
 app.use(cors());
+
+// connect mongodb
+connection();
 
 app.get("/", (req, res) => {
   res.send("Hello from server!");
 });
+
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
   console.log(`Welcome to my nodeServer on port http://localhost:${port}`);
