fc65d48db4ae395156618f5a01ff4ecfb2ad828e
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-08 03:23:22 +0545

    feat:add user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { loginHandler, signupHandler } from "../controllers/user.js";
+import {
+  loginHandler,
+  signupHandler,
+  updatePasswordHandler,
+  deleteUserHandler,
+} from "../controllers/user-auth.js";
 
+import { createAppointmentHandler } from "../controllers/appointmentDetails.js";
 const router = Router();
 
 router.post("/signup", signupHandler);
 router.post("/login", loginHandler);
+router.post("/update-password", updatePasswordHandler);
+router.post("/delete", deleteUserHandler);
 
+router.post("/create-appointment", createAppointmentHandler);
 export default router;
