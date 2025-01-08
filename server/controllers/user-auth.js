8661c71fa55e6752c59a5ca543b9ba9aa56f80c3
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-09 01:09:39 +0545

    feat:add jwt verification and send via cookies

--- server/controllers/user-auth.js
+++ server/controllers/user-auth.js
@@ -1,4 +1,5 @@
 import User from "../models/user/user.js";
+import { setUser } from "../utils/auth.js";
 
 export const signupHandler = async (req, res) => {
   const {
@@ -28,10 +29,9 @@ export const signupHandler = async (req, res) => {
       email,
       password,
     });
-    res.status(201).json({ message: "User created successfully" });
+    return res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-    res.end();
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -47,9 +47,12 @@ export const loginHandler = async (req, res) => {
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid password" });
     }
-    res.json({ message: "login sucessfull" });
+
+    const token = setUser(user);
+    res.cookie("token", token);
+    return res.status(200).json({ message: "Login successful" });
   } catch (error) {
-    console.log(error);
+    return res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -81,7 +84,6 @@ export const updatePasswordHandler = async (req, res) => {
 
     return res.status(200).json({ message: "Password updated successfully" });
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ message: "Server error" });
   }
 };
@@ -103,7 +105,6 @@ export const deleteUserHandler = async (req, res) => {
 
     return res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ message: "Server error" });
   }
 };
