b8a2598474fcc2bf3ebebf2ae8a967e774e9ca21
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-09 01:16:22 +0545

    feat:add additional middleware for checking authentication and sending jwt token via cookie and restrict users

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import UserRouter from "./routes/user.js";
 import connection from "./connection.js";
+import appointmentRouter from "./routes/appointment.js";
+import { checkForAuthentication, restrictTo } from "./middleware/auth.js";
 
 const app = express();
 const port = 3000;
@@ -10,6 +13,9 @@ const port = 3000;
 // Middleware setput
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthentication);
 
 // connect mongodb
 connection();
@@ -19,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", UserRouter);
+app.use("/appointment", restrictTo(["USER"]), appointmentRouter);
 
 app.listen(port, () => {
   console.log(`Welcome to my nodeServer on port http://localhost:${port}`);
