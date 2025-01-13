b15f1a8964abc6dff3eae77124470d2681a7812d
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-14 03:38:17 +0545

    refactor: move file to separate folder for role-based functionality organization

--- server/routes/appointment.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createAppointmentHandler } from "../controllers/appointmentDetails.js";
-
-const router = Router();
-router.post("/create-appointment", createAppointmentHandler);
-
-export default router;
